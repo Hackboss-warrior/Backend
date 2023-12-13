ef3babf4e84f7ac0bbfedff96c3b7ee9d75a3e4a
Skybrick <97291378+Skybrick@users.noreply.github.com>
2023-12-13 12:07:16 +0100

    UPD | filterPostbyTags

--- src/controllers/posts/createPost.js
+++ src/controllers/posts/createPost.js
@@ -1,6 +1,5 @@
 import { insertPost } from "../../models/news/index.js";
 import { createPostValidation } from "../../utils/joi.js";
-import generateError from "../../utils/generateError.js";
 import { v4 as uuidv4 } from 'uuid';
 import path from "path";
 import fs from "fs";
@@ -12,10 +11,9 @@ const createPost = async (req, res, next) => {
     const archivoSubido = req.files.image;
 
     const filePath = `./temp/${archivoSubido.name}`;
-    // Mover el archivo subido
     archivoSubido.mv(filePath, async (err) => {
       if (err) {
-        return next(generateError(err, 500, "Error al subir el archivo"));
+        return next(err);
       }
 
       try {
@@ -28,11 +26,9 @@ const createPost = async (req, res, next) => {
         fs.writeFileSync(newFilePath, imageBuffer);
 
         fs.unlinkSync(`./temp/${archivoSubido.name}`)
-
-        // Validar datos de entrada
+   
         createPostValidation({ title, topic, body, tags });
 
-        // Insertar el post en la base de datos
         await insertPost({ title, topic, uniqueFilename, body, tags, AuthUserId });
 
         res.send(title); 
--- src/models/news/filterPostByTags.js
+++ src/models/news/filterPostByTags.js
@@ -6,7 +6,7 @@ const filterPostByTags = async ({ etiqueta }) => {
   const conditions = etiqueta.map(etiqueta => `JSON_EXTRACT(tags, '$.${etiqueta}') = true`).join(' OR ');
   const queryString = `SELECT title, files, topic, body, tags, userId, createdAt FROM posts WHERE ${conditions}`;
   try {
-    const [rows, fields] = await pool.query(queryString);
+    const [rows] = await pool.query(queryString);
     return rows;
   } catch (error) {
     generateError(`Error al buscar posts por etiquetas:${error}`, 400)
