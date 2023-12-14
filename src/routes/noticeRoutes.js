5ec2d7b6389642a03675d69a3ec2f995ba96f814
canciox <canciox@gmail.com>
2023-12-14 13:49:36 +0100

    add noticeroutes

--- src/routes/noticeRoutes.js
+++ src/routes/noticeRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { createPost, lsPostById, deletePost, interactPost, getAllPosts, patchPost, filterPost,selectFavorites,insertFavorite } from "../controllers/posts/index.js";
+import { createPost, lsPostById, deletePost, interactPost, getAllPosts, patchPost, filterPost, selectFavorites, insertFavorite } from "../controllers/posts/index.js";
 import validateAuth from "../middlewares/validateAuth.js";
 
 const router = express.Router();
@@ -12,6 +12,6 @@ router.post("/liked", validateAuth, interactPost)
 router.patch("/post/:id", validateAuth, patchPost)
 router.post("/filter", filterPost)
 ////////////////////////////////////////
-router.post("/favorite/:id",validateAuth, insertFavorite)
-router.get("/favorites",validateAuth, selectFavorites)
+router.post("/favorite/:id", validateAuth, insertFavorite)
+router.get("/favorites", validateAuth, selectFavorites)
 export default router;
