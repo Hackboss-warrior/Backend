4b4bdd4bfaa68f894c07d559a6c31d6f1a71308f
Omar Díaz <omardiazglez@gmail.com>
2024-01-17 18:42:24 +0100

    UPD | listCommentsByPostId model

--- src/models/news/listCommentsByPostId.js
+++ src/models/news/listCommentsByPostId.js
@@ -2,7 +2,7 @@ import getPool from "../../db/pool.js";
 let pool = await getPool();
 
 const listCommentByPostId = async (id) => {
-  const [[resultado]] = await pool.query(
+  const [resultado] = await pool.query(
     "SELECT * FROM comments WHERE postId = ?",
     [id]
   );
